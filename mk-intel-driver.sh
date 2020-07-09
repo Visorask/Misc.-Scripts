2327b11c37d7d03bf901deadb0c6d5607ea4a648
Visorask <54461452+Visorask@users.noreply.github.com>
2020-07-08 23:33:29 -0500

    Update mk-intel-driver.sh

--- mk-intel-driver.sh
+++ mk-intel-driver.sh
@@ -7,7 +7,7 @@ echo "$(date "+%d.%m.%Y %T") INFO: *** Creating master directory. ***"
 mkdir intel-driver
 cd "$HOME/intel-driver"
 echo "$(date "+%d.%m.%Y %T") INFO: *** Installing prerequisites needed to build the iHD driver. ***"
-sudo apt-get install -y software-properties-common autoconf libtool libdrm-dev xorg xorg-dev openbox libx11-dev libgl1-mesa-glx libgl1-mesa-dev xutils-dev build-essential cmake
+sudo apt-get install -y software-properties-common vainfo autoconf libtool libdrm-dev xorg xorg-dev openbox libx11-dev libgl1-mesa-glx libgl1-mesa-dev xutils-dev build-essential cmake
 echo "$(date "+%d.%m.%Y %T") INFO: *** Cloning libva git. ***"
 git clone "https://github.com/intel/libva.git"
 cd "$HOME/intel-driver/libva"
@@ -33,5 +33,7 @@ sudo rm -rf "/usr/lib/x86_64-linux-gnu/dri/iHD_drv_video.so"
 sudo cp "$HOME/intel-driver/build_media/media_driver/iHD_drv_video.so" "/usr/lib/x86_64-linux-gnu/dri/"
 export "LIBVA_DRIVERS_PATH=/usr/lib/x86_64-linux-gnu/dri/"
 export "LIBVA_DRIVER_NAME=iHD"
+echo "$(date "+%d.%m.%Y %T") INFO: *** Script is now complete. ***"
 
+echo "$(date "+%d.%m.%Y %T") INFO: *** Running vainfo to verify script worked. ***"
 vainfo
