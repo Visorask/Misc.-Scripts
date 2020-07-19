ffd9de87c835b08f65bf2bb632f2d7f8c4292012
Visorask <54461452+Visorask@users.noreply.github.com>
2020-07-19 01:39:55 -0500

    Update mk-intel-driver.sh

--- mk-intel-driver.sh
+++ mk-intel-driver.sh
@@ -5,26 +5,35 @@ echo "$(date "+%d.%m.%Y %T") INFO: *** Initializing Script ***"
 cd "$HOME/"
 echo "$(date "+%d.%m.%Y %T") INFO: *** Creating master directory. ***"
 mkdir intel-driver
+sleep 5
 cd "$HOME/intel-driver"
 echo "$(date "+%d.%m.%Y %T") INFO: *** Installing prerequisites needed to build the iHD driver. ***"
 sudo apt-get install -y software-properties-common vainfo autoconf libtool libdrm-dev xorg xorg-dev openbox libx11-dev libgl1-mesa-glx libgl1-mesa-dev xutils-dev build-essential cmake
 echo "$(date "+%d.%m.%Y %T") INFO: *** Cloning libva git. ***"
 git clone "https://github.com/intel/libva.git"
+sleep 5
 cd "$HOME/intel-driver/libva"
 echo "$(date "+%d.%m.%Y %T") INFO: *** Building and installing libva (VAAPI). ***"
 ./autogen.sh --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu
+sleep 5
 make
+sleep 5
 sudo make install
+sleep 5
 
 # Building media driver
 cd $HOME/intel-driver
 echo "$(date "+%d.%m.%Y %T") INFO: *** Cloning media-driver and gmmlib gits. ***"
 git clone https://github.com/intel/media-driver.git
+sleep 5
 git clone https://github.com/intel/gmmlib.git
+sleep 5
 mkdir build_media
+sleep 5
 cd "$HOME/intel-driver/build_media"
 echo "$(date "+%d.%m.%Y %T") INFO: *** Building media-driver. ***"
 cmake -DCMAKE_INSTALL_PREFIX=/usr ../media-driver
+sleep 5
 make -j8
 
 # Removing and copying new driver.
